023d32df2d04d9cccf69f5f94bf9d2d538f22c07
RafeStefano <stefanorafe@gmail.com>
2021-03-02 00:11:49 +0300

    final unit + functional tests ready

--- hashthat/hashing/tests.py
+++ hashthat/hashing/tests.py
@@ -4,25 +4,25 @@ from .forms import HashForm
 import hashlib
 from .models import Hash
 
-# class FunctionalTestCase(TestCase):
+class FunctionalTestCase(TestCase):
     
-#     def setUp(self):
-#         self.browser = webdriver.Firefox()
+    def setUp(self):
+        self.browser = webdriver.Firefox()
     
-#     def test_there_is_homepage(self):
-#         self.browser.get('http://localhost:8000')
-#         self.assertIn('Enter hash here:', self.browser.page_source)
-#         # assert browser.page_source.find('install')
+    def test_there_is_homepage(self):
+        self.browser.get('http://localhost:8000')
+        self.assertIn('Enter hash here:', self.browser.page_source)
+        # assert browser.page_source.find('install')
     
-#     def test_hash_of_hello(self):
-#         self.browser.get('http://localhost:8000')
-#         text = self.browser.find_element_by_id('id_text')
-#         text.send_keys('hello')
-#         self.browser.find_element_by_name('submit').click()
-#         self.assertIn('2CF24DBA5FB0A30E26E83B2AC5B9E29E1B161E5C1FA7425E73043362938B9824', self.browser.page_source)
+    def test_hash_of_hello(self):
+        self.browser.get('http://localhost:8000')
+        text = self.browser.find_element_by_id('id_text')
+        text.send_keys('hello')
+        self.browser.find_element_by_name('submit').click()
+        self.assertIn('2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824', self.browser.page_source)
     
-#     def tearDown(self):
-#         self.browser.quit()
+    def tearDown(self):
+        self.browser.quit()
 
 
 class UnitTestCase(TestCase):

3ebb0dc081f585a4964ba1ddcb35e018c63b2bc1
RafeStefano <stefanorafe@gmail.com>
2021-03-02 00:22:20 +0300

    added bad data test

--- hashthat/hashing/tests.py
+++ hashthat/hashing/tests.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 from .forms import HashForm
 import hashlib
 from .models import Hash
+from django.core.exceptions import ValidationError
 
 class FunctionalTestCase(TestCase):
     
@@ -63,3 +64,10 @@ class UnitTestCase(TestCase):
         hash = self.saveHash()
         response = self.client.get('/hash/2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824')
         self.assertContains(response, 'hello')
+        
+    def test_bad_data(self):
+        def badHash():
+            hash = Hash()
+            hash.hash = '2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824gg'
+            hash.full_clean()
+        self.assertRaises(ValidationError, badHash)

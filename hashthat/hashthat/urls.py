3e7221a4237602e606baf68645b3972f5e63b38a
RafeStefano <stefanorafe@gmail.com>
2021-03-02 00:08:43 +0300

    added hash page

--- hashthat/hashthat/urls.py
+++ hashthat/hashthat/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from os import name
 from django.contrib import admin
 from django.urls import path
 from hashing import views
@@ -20,4 +21,5 @@ from hashing import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
+    path('hash/<str:hash>', views.hash, name='hash'),
 ]
